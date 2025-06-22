ac2926d8c2e938b2cd7c6180e114936e39670230
Owen Erhabor <erhaborowen3@gmail.com>
2025-06-22 22:04:29 +0100

    Refactor navigation and enhance styles for improved user experience

--- js/script.js
+++ js/script.js
@@ -3,4 +3,26 @@ import typewriter from "./typewriter.js";
 const heading = document.querySelector("#introduction");
 const text = "Welcome To My Portfolio: An introduction";
 
-typewriter(heading, text, 70);
+typewriter(heading, text, 50);
+
+const navbarOverlay = document.querySelector("#navbar-overlay");
+const navbarList = document.querySelector("#nav-list");
+const menuToggle = document.querySelector("#menu-toggle");
+
+function waitForAnimationEnd(element) {
+    return new Promise(resolve => {
+        const handleAnimationEnd = () => {
+            element.removeEventListener('animationend', handleAnimationEnd);
+            resolve();
+        };
+        element.addEventListener('animationend', handleAnimationEnd);
+    });
+}
+
+async function handleMenuTogle() {
+    navbarList.classList.add("swoop-in");
+    await waitForAnimationEnd(navbarList);
+    navbarOverlay.classList.add("swoop-in");
+}
+
+menuToggle.addEventListener("click", handleMenuTogle);
